1097859b91da7b1f6300828546f85f8eae860eb8
Your Name <you@example.com>
2022-11-26 20:10:49 +0530

    Added: Welcome Message

---
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class BinarySearchTree {
+
+    public static void main(String[] args) {
+
+        System.out.println("....Welcome To Binary Serach Tree Program....");
+    }
+}
